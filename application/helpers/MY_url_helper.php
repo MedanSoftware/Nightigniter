76f21e1deabfe9e6750520ed95e25519e3451481
Agung Dirgantara <agungmasda29@gmail.com>
2020-02-17 19:53:19 +0700

    update URL helper

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -6,6 +6,51 @@
  * @author Agung Dirgantara <agungmasda29@gmail.com>
  */
 
+/**
+ * Site URL
+ *
+ * Create a local URL based on your basepath. Segments can be passed via the
+ * first parameter either as a string or an array.
+ *
+ * @param	string	$uri
+ * @param   boolean $query_string
+ * @param	string	$protocol
+ * @return	string
+ */
+function site_url($uri = '', $query_string = false, $protocol = null)
+{
+	return get_instance()->config->site_url($uri, $protocol).(filter_var($query_string, FILTER_VALIDATE_BOOLEAN)?get_http_build_query():FALSE);
+}
+
+
+/**
+ * Base URL
+ *
+ * Create a local URL based on your basepath.
+ * Segments can be passed in as a string or an array, same as site_url
+ * or a URL to a file can be passed in, e.g. to an image file.
+ *
+ * @param	string	$uri
+ * @param   boolean $query_string
+ * @param	string	$protocol
+ * @return	string
+ */
+function base_url($uri = '', $query_string = false, $protocol = null)
+{
+	return get_instance()->config->base_url($uri, $protocol).(filter_var($query_string, FILTER_VALIDATE_BOOLEAN)?get_http_build_query():FALSE);
+}
+
+/**
+ * Current URL
+ * 
+ * @param  boolean $query_string
+ * @return string
+ */
+function current_url($query_string = true)
+{
+	return base_url(uri_string().config_item('url_suffix')).(filter_var($query_string, FILTER_VALIDATE_BOOLEAN)?get_http_build_query():FALSE);
+}
+
 if (!function_exists('api_url'))
 {
 	/**
@@ -30,20 +75,6 @@ if (!function_exists('get_http_build_query'))
 	function get_http_build_query()
 	{
 		return (!empty(get_instance()->input->get()))?'?'.http_build_query(get_instance()->input->get()):FALSE;
-	}
-}
-
-if (!function_exists('get_current_url'))
-{
-	/**
-	 * Current URL
-	 * 
-	 * @param  boolean $http_build_query with the query params
-	 * @return string
-	 */
-	function get_current_url($http_build_query = true)
-	{
-		return ($http_build_query)?base_url(uri_string().config_item('url_suffix')).get_http_build_query():base_url(uri_string().config_item('url_suffix'));
 	}
 }
 
